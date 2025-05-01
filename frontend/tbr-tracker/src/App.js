a73a0e5861f2b6d5ebfff7119a92bbba900b3033
JustinA02-N <justinalvarado.n@gmail.com>
2025-05-01 00:34:56 -0500

    Add built React app

--- frontend/tbr-tracker/src/App.js
+++ frontend/tbr-tracker/src/App.js
@@ -1,33 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { ThemeProvider, CssBaseline, AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import theme from './theme'; 
+
 import BookList from './components/BookList';
 import BookDetail from './components/BookDetail';
 import BookForm from './components/BookForm';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Navbar, Nav } from 'react-bootstrap'; 
 
 function App() {
   return (
-    <Router>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">TBR Tracker</Navbar.Brand>
-          <Nav className="ms-auto">
-            <Nav.Link href="/add">Add New Book</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              TBR Tracker
+            </Typography>
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/add">
+              Add Book
+            </Button>
+          </Toolbar>
+        </AppBar>
 
-      <Container className="py-4">
-        <Routes>
-          <Route path="/" element={<BookList />} />
-          <Route path="/books" element={<BookList />} />
-          <Route path="/books/:id" element={<BookDetail />} />
-          <Route path="/add" element={<BookForm />} />
-          <Route path="/edit/:id" element={<BookForm />} />
-        </Routes>
-      </Container>
-    </Router>
+        <Container sx={{ py: 4 }}>
+          <Routes>
+            <Route path="/" element={<BookList />} />
+            <Route path="/books" element={<BookList />} />
+            <Route path="/books/:id" element={<BookDetail />} />
+            <Route path="/add" element={<BookForm />} />
+            <Route path="/edit/:id" element={<BookForm />} />
+          </Routes>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
 }
 
